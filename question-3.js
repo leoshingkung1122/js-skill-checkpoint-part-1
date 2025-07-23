b125dcb3bf24c8d2e4db46ab655692836ed9806f
Shin <sirapop1998@gmail.com>
2025-07-23 13:54:38 +0700

    feat: add answer to question-3

--- question-3.js
+++ question-3.js
@@ -1,3 +1,24 @@
 // Question #3
 let userPassword = "";
 // เริ่มเขียนโค้ดตรงนี้
+function checkPasswordStrength(password) {
+    if (password.length < 6) {
+        return "Weak";
+    } else if (password.length >=6 && password.length <=10) {
+        return "Medium";
+    } else if (password.length > 10) {
+        return "Strong";
+    } else {
+        return "N/A";
+    }
+
+}
+
+userPassword = "ssswnalWadqQ"
+console.log(checkPasswordStrength(userPassword));
+
+userPassword = "TechUp"
+console.log(checkPasswordStrength(userPassword));
+
+userPassword = "abcde"
+console.log(checkPasswordStrength(userPassword)); 
